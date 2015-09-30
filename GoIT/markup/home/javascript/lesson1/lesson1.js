5f22cd5326aee1bd4dd5ae345fd30848a505be1e
serhiitolstoi <serhiitolstoi@gmail.com>
2015-09-30 19:34:18 +0300

    lesson1 (var prompt alert)
    lesson1 (var prompt alert)

---
+++ GoIT/markup/home/javascript/lesson1/lesson1.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Serhii on 30.09.2015.
+ */
+
+var developer;
+var name;
+name='Sergey';
+developer=name;
+console.log(developer);
+
+var enterName=prompt('Enter your name, Amigo!');
+alert('Happy to see you, '+enterName+' !');
