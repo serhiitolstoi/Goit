e5537a4a60c14e3948ecf6fddae761002cefa114
serhiitolstoi <serhiitolstoi@gmail.com>
2015-11-02 21:59:46 +0200

    lesson6 (arrays)
    lesson6 (arrays)

---
+++ GoIT/javascript/lesson6/task1.js
@@ -0,0 +1,25 @@
+'use strict'
+
+//У объекта есть свойство className, которое хранит список css класов – слов, разделенных пробелами
+//Напишите функцию removeClass(obj, cls), которая удаляет класс cls, если он есть
+//P.S. Дополнительное усложнение. Функция должна корректно обрабатывать дублирование класса в строке
+
+var obj = {
+    className: 'open menu menu sim'
+};
+
+function  removeClass(obj, cls) {
+    var arr = obj.className.split(' ');
+
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] == cls) {
+            arr.splice (i, 1);
+            i--;
+        }
+    }
+
+    obj.className = arr.join(' ');
+}
+
+removeClass(obj, 'menu');
+alert(obj.className);
---
+++ GoIT/javascript/lesson6/task2.js
@@ -0,0 +1,14 @@
+'use strict'
+
+//Есть массив строк arr. Создайте массив arrSorted — из тех же элементов, но отсортированный.
+//Исходный массив не должен меняться.
+
+var arr = ['HTML', 'JavaScript', 'CSS'];
+var arrSorted = [];
+for (var i = 0; i < arr.length; i++) {
+    arrSorted[i] = arr[i];
+    arrSorted.sort();
+}
+
+console.log(arr);
+console.log(arrSorted);
