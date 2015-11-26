72999868b7b145f6ca7fc88037f690ec45666e0d
serhiitolstoi <serhiitolstoi@gmail.com>
2015-11-26 14:42:13 +0200

    lesson14 (proto OOP)
    lesson14 (proto OOP)

---
+++ GoIT/javascript/lesson14/task5.js
@@ -0,0 +1,54 @@
+'use strict';
+
+function Hamster() {
+    this.food = [];
+}
+
+Hamster.prototype.found = function(something) {
+    this.food.push(something);
+};
+
+var speedy = new Hamster();
+var lazy = new Hamster();
+
+speedy.found("яблоко");
+speedy.found("орех");
+
+alert( speedy.food.length );
+alert( lazy.food.length );
+
+
+
+/*
+ Вы — руководитель команды, которая разрабатывает игру, хомяковую ферму.
+ Один из программистов получил задание создать класс «хомяк» (англ – "Hamster").
+
+ Объекты-хомяки должны иметь массив food для хранения еды и метод found, который добавляет к нему.
+
+ Ниже — его решение. При создании двух хомяков, если поел один — почему-то сытым становится и второй тоже.
+
+ В чём дело? Как поправить?
+
+ function Hamster() {}
+
+ Hamster.prototype.food = []; // пустой "живот"
+
+ Hamster.prototype.found = function(something) {
+ this.food.push(something);
+ };
+
+ // Создаём двух хомяков и кормим первого
+ var speedy = new Hamster();
+ var lazy = new Hamster();
+
+ speedy.found("яблоко");
+ speedy.found("орех");
+
+ alert( speedy.food.length ); // 2
+ alert( lazy.food.length ); // 2 (!??)
+
+ */
+
+
+
+
